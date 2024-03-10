667a84443786f7c9e417741538ec04ac6181f6aa
suyash soni <suyashsoni983@gmail.com>
2024-03-11 00:01:30 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const box=document.querySelector(".change");
+box.addEventListener('click',()=>{
+    var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor=randomColor;
+})
+  
